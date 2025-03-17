214ba40ef6ff5a86fa92ca32f8da8b1abf8d9348
SchwarzRene <20191028@atn.ac.at>
2025-03-17 20:23:54 +0100

    getting to know the constructor

--- lunarlander/src/main.rs
+++ lunarlander/src/main.rs
@@ -10,17 +10,44 @@ struct Lander{
     y_velocity: f64,
 }
 
+impl Lander {
+    fn slowdown( &mut self ){
+        self.x_velocity = self.x_velocity - 1.;
+        self.y_velocity = self.y_velocity - 1.;
+    }
+}
+
+impl Lander {
+    fn upordown( &self ) -> bool{
+        self.y_velocity > 0.
+    }
+}
+
+impl Lander {
+    fn standing() -> Self{
+        Self{
+            pos: Position{ x_coordinate: 0., y_coordinate: 0. },
+            x_velocity: 0.,
+            y_velocity: 0.,
+        }
+    }
+}
+
 fn main(){
     println!( "Lunar Lander" );
     println!( "Playing with Structs" );
 
-    let position = Position { x_coordinate: 10., y_coordinate: 10. };
-    let lander = Lander{ pos: position, x_velocity: 10., y_velocity: 10. };
+    let mut lander = Lander::standing();
 
     change_speed(&lander);
     change_position(&lander.pos);
 
     println!( "Lander: {lander:#?}");
+
+    lander.slowdown();
+    lander.upordown();
+
+    println!( "Lander: {lander:#?}" );
 }
 
 fn change_speed( lander : &Lander ) -> f64{
