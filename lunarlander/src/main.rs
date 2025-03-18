5fa51fe6f57f431c9aa560fa51bbb9554a4d7eed
SchwarzRene <20191028@atn.ac.at>
2025-03-18 15:01:56 +0100

    summary

--- lunarlander/src/main.rs
+++ lunarlander/src/main.rs
@@ -1,59 +1,29 @@
 #[derive(Debug)]
-struct Position{
-    x_coordinate: f64,
-    y_coordinate: f64,
-}
-#[derive(Debug)]
-struct Lander{
-    pos: Position,
-    x_velocity: f64,
-    y_velocity: f64,
+struct Table{
+    width : f64,
+    height : f64,
+    deepth : f64,
 }
 
-impl Lander {
-    fn slowdown( &mut self ){
-        self.x_velocity = self.x_velocity - 1.;
-        self.y_velocity = self.y_velocity - 1.;
+impl Table{
+    fn volume( &mut self ) -> f64 {
+        self.width = self.width + 1.;
+        self.width * self.height * self.deepth
     }
-}
 
-impl Lander {
-    fn upordown( &self ) -> bool{
-        self.y_velocity > 0.
-    }
-}
-
-impl Lander {
-    fn standing() -> Self{
-        Self{
-            pos: Position{ x_coordinate: 0., y_coordinate: 0. },
-            x_velocity: 0.,
-            y_velocity: 0.,
+    fn table( width : f64, height : f64 ) -> Table{
+        Table{ 
+            width : width,
+            height : height,
+            deepth : 100.,
         }
     }
 }
 
 fn main(){
-    println!( "Lunar Lander" );
-    println!( "Playing with Structs" );
+    let t = Table::table( 100., 200. );
+    let v = t.volume();
 
-    let mut lander = Lander::standing();
-
-    change_speed(&lander);
-    change_position(&lander.pos);
-
-    println!( "Lander: {lander:#?}");
-
-    lander.slowdown();
-    lander.upordown();
-
-    println!( "Lander: {lander:#?}" );
-}
-
-fn change_speed( lander : &Lander ) -> f64{
-    lander.x_velocity + lander.y_velocity
-}
-
-fn change_position( position : &Position ) -> f64{
-    position.x_coordinate + position.y_coordinate
+    println!( "{t:#?}" );
+    println!( "{v}" ); 
 }
